2565aef7212db7317568de75e74fa518943f1680
Andrew <andrew3840@gmail.com>
2025-11-10 16:25:48 -0500

    Resolved vite dependency issues

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Refurbished-Computer-Store/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
